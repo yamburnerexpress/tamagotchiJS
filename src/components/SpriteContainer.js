a36127e52584edbcfe292fa1522c7b8c613380b3
yamburnerexpress <kevinnorwood92@gmail.com>
2024-04-06 11:45:51 -0500

    init status icons

--- src/components/SpriteContainer.js
+++ src/components/SpriteContainer.js
@@ -1,12 +1,13 @@
 import React from "react";
 import '../css/display.css'
 import { Status } from "./Status";
+import {StatusIconGroup} from "./StatusIconGroup"
 
 export const SpriteContainer = () => {
     return (
         <div className="spriteContainer">
             <div className="topPanel">
-                <div className="statusIcons"></div>
+                <StatusIconGroup />
                 <div className="sprite"></div>
                 <Status />
             </div>
